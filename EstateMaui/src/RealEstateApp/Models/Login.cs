8c62a2fd83641db25b9e0726ca5ab6881434354d
Fu Xu <xufu202@gmail.com>
2023-06-12 00:49:21 +0100

    create maui

---
+++ EstateMaui/src/RealEstateApp/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System;
+namespace RealEstateApp.Models
+{
+	public class Login
+	{
+		public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+
+        public string? Name { get; set; } 
+        //public string Email { get; set; }
+        //public string Password { get; set; }
+        public string? Phone { get; set; } 
+    }
+}
+
